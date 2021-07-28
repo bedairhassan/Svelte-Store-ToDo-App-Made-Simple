f6c21aff659b9ec358aff8a9d282385c217c48f6
hassan <brhdtqiuiiivdzey@yandex.com>
2021-07-28 02:05:18 +0200

    checkpoint0

---
+++ src/App.svelte
@@ -0,0 +1,33 @@
+<script>
+	import { items } from "./store";
+
+
+	let display;
+	$: items.subscribe(last=>display=last)
+
+	let item;
+	const submit = () => items.update((last) => [...last, item]);
+</script>
+
+<main>
+	<table>
+		<thead>
+			<th>Name of Task</th>
+		</thead>
+
+		<tbody>
+			{#if display.length>0}
+				{#each display as item}
+					<tr>
+						<td>{item}</td>
+					</tr>
+				{/each}
+			{/if}
+
+			<tr>
+				<td><input type="text" bind:value={item} /></td>
+				<td><button on:click={submit}>add task</button></td>
+			</tr>
+		</tbody>
+	</table>
+</main>
